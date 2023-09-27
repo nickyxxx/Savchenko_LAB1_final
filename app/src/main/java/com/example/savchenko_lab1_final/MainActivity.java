5b5bf1f9ef024952124d83efb7ffa4c05208a18a
klym4uk <klym4uk.ant0n@gmail.com>
2023-09-27 13:57:20 +0300

    "Change text" button

--- app/src/main/java/com/example/savchenko_lab1_final/MainActivity.java
+++ app/src/main/java/com/example/savchenko_lab1_final/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.savchenko_lab1_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void textChanger(View view)
+    {
+        TextView text = (TextView) findViewById(R.id.text1);
+        if(text.getText() == "Welcome!") text.setText("HURRRAYYY!!!");
+        else text.setText("Welcome!");
+    }
+
 }
