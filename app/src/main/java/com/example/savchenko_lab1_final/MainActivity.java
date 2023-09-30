ddcd8e385f7ebda88e41d14776697aa3b40e228e
nickyxxx <nicksavchenko05@gmail.com>
2023-09-30 16:03:42 +0300

    Revert ""Change color" button"
    This reverts commit 7bfdacd86c53e9a4b1fe84cd025d09bad7ecf32f.

--- app/src/main/java/com/example/savchenko_lab1_final/MainActivity.java
+++ app/src/main/java/com/example/savchenko_lab1_final/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.savchenko_lab1_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -22,17 +20,5 @@ public class MainActivity extends AppCompatActivity {
         if(text.getText() == "Welcome!") text.setText("HURRRAYYY!!!");
         else text.setText("Welcome!");
     }
-
-   public void colorChanger (View view)
-    {
-        TextView text = (TextView) findViewById(R.id.text1);
-        if(text.getCurrentTextColor() == Color.parseColor("#0F9D58"))
-        {
-            text.setTextColor(Color.parseColor("#FF0000"));
-        }
-        else text.setTextColor(Color.parseColor("#0F9D58"));
-
-    }
-
 
 }
